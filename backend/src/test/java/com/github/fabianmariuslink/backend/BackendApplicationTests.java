cbd235ee3486fa36d1d7e17cc06e47d9b1f04d4b
FabianMariusLink <fabian.marius.link@gmail.com>
2024-01-09 08:21:44 +0100

    Add backend module

---
+++ backend/src/test/java/com/github/fabianmariuslink/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fabianmariuslink.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
